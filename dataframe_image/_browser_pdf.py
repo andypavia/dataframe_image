1da63445394b5587cbdf52ff2609e3bca61f7a15
andypavia <pavia.andres@gmail.com>
2021-08-18 11:17:48 +0200

    adding no-sandbox for testing

--- dataframe_image/_browser_pdf.py
+++ dataframe_image/_browser_pdf.py
@@ -72,7 +72,8 @@ def launch_chrome():
         '--headless',
         '--disable-gpu', 
         '--run-all-compositor-stages-before-draw',
-        '--remote-debugging-port=9222'
+        '--remote-debugging-port=9222',
+        '--no-sandbox'
     ]
     p = Popen(args=args)
     return p
